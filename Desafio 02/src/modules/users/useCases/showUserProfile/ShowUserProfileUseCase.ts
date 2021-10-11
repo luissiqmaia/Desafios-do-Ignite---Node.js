7c1198922c1da3bab76cc9e1f5b9aaf8fff0cba5
Luís Henrique <luismaya78@gmail.com>
2021-10-10 22:59:23 -0300

    Add another challenge

---
+++ Desafio 02/src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -0,0 +1,19 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ShowUserProfileUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) throw new Error("User's profile do not exists.");
+    return user;
+  }
+}
+
+export { ShowUserProfileUseCase };
